aee17a9983ee59a646a75e401d03d101f9d7b833
Yuze Lang <ylang@twitter.com>
2018-04-04 18:31:24 -0700

    Problem ======= Edge disallows drag event by default (A No Symbol (⃠) will be shown on the dragged image by default).. Drop event is not triggered.
    Solution
    ========
    Prevent the default handler of dragOver event would make the drop event go through.
    The underlying hypothesis is somehow in Edge, the default handler of dragOver affects drop event (Note draft-js already calls `e.preventDefault` in `onDrop`).

--- src/component/handlers/drag/DraftEditorDragHandler.js
+++ src/component/handlers/drag/DraftEditorDragHandler.js
@@ -136,6 +136,14 @@ const DraftEditorDragHandler = {
     }
     endDrag(editor);
   },
+
+  /**
+   * Edge disallows drag event by default (A No Symbol (⃠) will be shown on the dragged image by default).
+   * Without preventing the default dragOver handler a drop event can not be triggered.
+   */
+  onDragOver: function onDragOver(editor: DraftEditor, e: Object): void {
+    e.preventDefault();
+  },
 };
 
 function endDrag(editor) {
